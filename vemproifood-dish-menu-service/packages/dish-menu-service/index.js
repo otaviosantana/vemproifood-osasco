e9ad154ca1a29ea2797a4b5668b3f4801af0e241
jasydafny <jasy.silva@outlook.com>
2018-11-21 21:42:28 -0200

    add restaurants endpoint

--- vemproifood-dish-menu-service/packages/dish-menu-service/index.js
+++ vemproifood-dish-menu-service/packages/dish-menu-service/index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const csv = require('fast-csv');
 
 const orders = []
+const restaurants = []
 
 csv
   .fromPath('../../../orders.csv')
@@ -21,10 +22,31 @@ csv
     console.log('pedidos carregados');
   });
 
+  csv
+  .fromPath('../../../restaurantes.csv')
+  .on('data', data => {
+    const [ restaurant, dish ] = data[0].split(';')
+    
+    if (restaurant === 'restaurant') return true;
+    
+    orders.push({
+      restaurant, dish
+    })
+  })
+  .on('end', () => {
+    console.log('restaurantes carregados');
+  });
+
 app.get('/dish/:dishType', function (req, res) {
   const dishType = req.params.dishType
   const ordersByDishType = orders.filter(dish => dish.dish_type === dishType)
   res.send(ordersByDishType);
+});
+
+app.get('/restaurantByDish/:dish', function (req, res) {
+  const dish = req.params.dish
+  const ordersByDish = orders.filter(dish => dish.dish === dish)
+  res.send(ordersByDish);
 });
 
 app.listen(3000, function () {
