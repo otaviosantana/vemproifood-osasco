d16b06953b04f1211b963e22dffabd46338b452d
Otavio Santana <osantana@bancocbss.com.br>
2018-11-21 20:02:10 -0200

    Create menu service project

---
+++ vemproifood-menu-service/src/main/java/br/com/ifood/vemproifood/vemproifoodmenuservice/VemproifoodMenuServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.ifood.vemproifood.vemproifoodmenuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemproifoodMenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VemproifoodMenuServiceApplication.class, args);
+	}
+}
